1d5bf55966f46a2155fdde7c6578d626cad62e90
Siobhán O'Hanlon <siobhanhanlon@outlook.com>
2022-02-09 15:29:34 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title:string = 'my-first-app';
+  name:string = "Martin";
+  age:number = 22;
 }
